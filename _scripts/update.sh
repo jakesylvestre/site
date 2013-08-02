075a7c0735c4b5a36fd89bb4bc1c39b8dda00476
wei2912 <wei2912.supp0rt@gmail.com>
2013-08-02 21:46:10 +0800

    Update script.

--- _scripts/update.sh
+++ _scripts/update.sh
@@ -1,8 +1,9 @@
 noGenerate="True" # set to true
+HOME="/var/otherusers/wei2912"
 # updating repo
-cd /var/otherusers/wei2912/site/
-touch /var/otherusers/wei2912/temp.txt
-/usr/bin/git pull > /var/otherusers/wei2912/temp.txt || exit 1
+cd $HOME/site
+touch $HOME/temp.txta
+/usr/bin/git pull > $HOME/temp.txt || exit 1
 
 echo $1
 if [ "$1" == "--force" ]
@@ -10,7 +11,7 @@ then
 	generate="True"
 else
 	# detect any changes
-    changes=$(grep -Fx 'Already up-to-date.' /var/otherusers/wei2912/temp.txt)
+    changes=$(grep -Fx 'Already up-to-date.' $HOME/temp.txt)
 	if [ -z "$changes" ]
 	then
 		echo "Grep result shows that site is not up to date."
@@ -23,12 +24,12 @@ else
 	if [ "$generate" == "False" ] # if it's not going to generate, try to get the script to generate the site again.
 	then
 		# checking for binaries
-		echo "$(tar c /var/otherusers/wei2912/mysite_files | md5sum)" > /var/otherusers/wei2912/temp_checksum.txt
-		if [ "$(sed '$!d' /var/otherusers/wei2912/temp_checksum.txt)" == "$(sed -n '1p' /var/otherusers/wei2912/files_checksum.txt)" ]
+		echo "$(tar c $HOME/mysite_files | md5sum)" > $HOME/temp_checksum.txt
+		if [ "$(sed '$!d' $HOME/temp_checksum.txt)" == "$(sed -n '1p' $HOME/files_checksum.txt)" ]
 		then
 			echo "Checksum shows that site is up to date."
 		else
-			echo "$(sed '$!d' /var/otherusers/wei2912/temp_checksum.txt)" > /var/otherusers/wei2912/files_checksum.txt
+			echo "$(sed '$!d' $HOME/temp_checksum.txt)" > $HOME/files_checksum.txt
 			echo "Checksum shows that site is not up to date."
 			generate="True"
 		fi
@@ -39,15 +40,15 @@ if [ "$generate" == "True" ]
 then
 	echo "True"
 	# copying the files over to mysite folder!
-	/var/otherusers/wei2912/.gem/ruby/1.9.1/gems/jekyll-*/bin/jekyll build  /var/otherusers/wei2912/site/_site/ || exit 1
-	rm -rf /var/otherusers/wei2912/mysite/*
-    cp -r /var/otherusers/wei2912/site/_site/* /var/otherusers/wei2912/mysite/
+	$HOME/.gem/ruby/1.9.1/gems/jekyll-*/bin/jekyll build  $HOME/site/_site/ || exit 1
+	rm -rf $HOME/mysite/*
+    cp -r $HOME/site/_site/* $HOME/mysite/
 else
 	echo "False"
 fi
 
-cp -r /var/otherusers/wei2912/mysite_files/* /var/otherusers/wei2912/mysite
+cp -r $HOME/mysite_files/* $HOME/mysite
 
 # clean up
-rm /var/otherusers/wei2912/temp.txt
-cd /var/otherusers/wei2912/
+rm $HOME/temp.txt
+cd $HOME/
