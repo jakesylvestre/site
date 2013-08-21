dc73379d9d05bac8c436f83b123c87016bdff4f0
wei2912 <wei2912.supp0rt@gmail.com>
2013-08-21 22:16:16 +0800

    Add new key generation mechanism.

--- _scripts/after_success.sh
+++ _scripts/after_success.sh
@@ -7,7 +7,12 @@ echo "    StrictHostKeyChecking no" >> ~/.ssh/config
 echo "    CheckHostIP no" >> ~/.ssh/config
 echo "    UserKnownHostsFile=/dev/null" >> ~/.ssh/config
 
-echo -n "$id_rsa_{1..5}" >> ~/.ssh/id_rsa
-chmod 0700 ~/.ssh/id_rsa
+ID_RSA_INFO=$(cat _scripts/id_rsa.info)
+ID_RSA_PUB_INFO=$(cat _scripts/id_rsa.pub.info)
+
+echo "$ID_RSA_{0..$ID_RSA_INFO}" >> ~/.ssh/id_rsa
+echo -n "$ID_RSA_PUB_{0..$ID_RSA_PUB_INFO}" >> ~/.ssh/id_rsa.pub
+chmod 0700 ~/.ssh/id_rsa ~/.ssh/id_rsa.pub
+
 rsync -avze ssh ./_site/* deploy_server:~/mysite/
 ssh deploy_server chmod -R 755 /var/otherusers/wei2912/mysite/
