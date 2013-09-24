3661af8da264f59de1dfde6a88a5aadd8385fbca
wei2912 <wei2912.supp0rt@gmail.com>
2013-09-24 19:07:49 +0800

    Add error handling; exit with error code 1 if something goes wary.

--- _scripts/after_success.sh
+++ _scripts/after_success.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-echo "Host deploy_server" >> ~/.ssh/config
-echo "    Hostname ${SSH_HOST}" >> ~/.ssh/config
-echo "    User ${SSH_USER}" >> ~/.ssh/config
-echo "    Port ${SSH_PORT}" >> ~/.ssh/config
-echo "    StrictHostKeyChecking no" >> ~/.ssh/config
-echo "    CheckHostIP no" >> ~/.ssh/config
-echo "    UserKnownHostsFile=/dev/null" >> ~/.ssh/config
-echo "    IdentityFile ~/.ssh/id_rsa" >> ~/.ssh/config
+echo "Host deploy_server" >> ~/.ssh/config || exit 1
+echo "    Hostname ${SSH_HOST}" >> ~/.ssh/config || exit 1
+echo "    User ${SSH_USER}" >> ~/.ssh/config || exit 1
+echo "    Port ${SSH_PORT}" >> ~/.ssh/config || exit 1
+echo "    StrictHostKeyChecking no" >> ~/.ssh/config || exit 1
+echo "    CheckHostIP no" >> ~/.ssh/config || exit 1
+echo "    UserKnownHostsFile=/dev/null" >> ~/.ssh/config || exit 1
+echo "    IdentityFile ~/.ssh/id_rsa" >> ~/.ssh/config || exit 1
 
-gpg --passphrase ${ENCRYPT_PASS} --output ~/.ssh/id_rsa --decrypt ./_scripts/travis_rsa.gpg
-gpg --passphrase ${ENCRYPT_PASS} --output ~/.ssh/id_rsa.pub --decrypt ./_scripts/travis_rsa.pub.gpg
-chmod 0700 ~/.ssh/id_rsa ~/.ssh/id_rsa.pub
+gpg --passphrase ${ENCRYPT_PASS} --output ~/.ssh/id_rsa --decrypt ./_scripts/travis_rsa.gpg || exit 1
+gpg --passphrase ${ENCRYPT_PASS} --output ~/.ssh/id_rsa.pub --decrypt ./_scripts/travis_rsa.pub.gpg || exit 1
+chmod 0700 ~/.ssh/id_rsa ~/.ssh/id_rsa.pub || exit 1
 
-rsync -avze ssh ./_site/* deploy_server:~/mysite/
-ssh deploy_server chmod -R 755 /var/otherusers/wei2912/mysite/
+rsync -avze ssh ./_site/* deploy_server:~/mysite/ || exit 1
+ssh deploy_server chmod -R 755 /var/otherusers/wei2912/mysite/ || exit 1
