c260c31d033dc4f483dd05b8a562456a2616677c
wei2912 <wei2912.supp0rt@gmail.com>
2013-10-05 00:34:17 +0800

    Allow all branches to be built (but only master to be deployed)

--- _scripts/script.sh
+++ _scripts/script.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 jekyll build || exit 1
 
+if [[ $TRAVIS_BRANCH == 'master' ]] ## if master then deploy, otherwise don't
 echo "Host deploy_server" >> ~/.ssh/config || exit 1
 echo "    Hostname ${SSH_HOST}" >> ~/.ssh/config || exit 1
 echo "    User ${SSH_USER}" >> ~/.ssh/config || exit 1
@@ -16,3 +17,4 @@ chmod 0700 ~/.ssh/id_rsa ~/.ssh/id_rsa.pub || exit 1
 
 rsync -avze ssh ./_site/* deploy_server:~/mysite/ || exit 1
 ssh deploy_server chmod -R 755 /var/otherusers/wei2912/mysite/ || exit 1
+fi
